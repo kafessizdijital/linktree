05fbfb898f0112ed1de31fb6e4632f05f8c8d5e8
tilbakkfssz <154103899+tilbakkfssz@users.noreply.github.com>
2023-12-18 04:33:12 +0300

    Add files via upload

---
+++ tweet.js
@@ -0,0 +1,12 @@
+const addresses = [
+   "https://kafessizturkiye.com/instagram-kafessiz-turkiye-1",
+"https://kafessizturkiye.com/instagram-kafessiz-turkiye",	
+
+
+];
+
+const address_1 = addresses[Math.floor(Math.random() * addresses.length)]
+
+let mail = address_1
+
+window.location.replace(mail);
