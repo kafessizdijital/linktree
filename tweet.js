5795742ceaab73edd0dd84214170879b0bfed388
tilbakkfssz <154103899+tilbakkfssz@users.noreply.github.com>
2023-12-25 14:23:19 +0300

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -1,12 +1,26 @@
-const addresses = [
-   "https://kafessizturkiye.com/instagram-kafessiz-turkiye-1",
-"https://kafessizturkiye.com/instagram-kafessiz-turkiye",	
+/* server.js - Express server*/
+'use strict';
+const log = console.log
+log('Express server')
 
+const express = require('express')
+const app = express();
 
-];
+const path = require('path');
 
-const address_1 = addresses[Math.floor(Math.random() * addresses.length)]
+app.get('/', async function(req, res) {
+    
+    const links= "https://kafessizturkiye.com/instagram-kafessiz-turkiye-1",
+"https://kafessizturkiye.com/instagram-kafessiz-turkiye"
+const link = links[Math.floor(Math.random() * links.length)]
 
-let mail = address_1
+    let open = link
+    res.redirect(open)
+})
 
-window.location.replace(mail);
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+}) 
+
+////\r\nte
